a14a136c5a8f7efc07986ae580f533a4d3a44297
Xiaofei <13263397018@163.com>
2019-12-11 11:13:19 +0800

    add springcloud demo

---
+++ service-zuul/src/main/java/com/taikang/servicezuul/filter/MyFilter.java
@@ -0,0 +1,50 @@
+package com.taikang.servicezuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    private  static Logger logger = LoggerFactory.getLogger(MyFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    // 这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info(String.format("%s>>>%s",request.getMethod(),request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null){
+            logger.warn("token is empty!");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try{
+                ctx.getResponse().getWriter().write("token is empty!");
+            }catch (IOException e){}
+            return null;
+        }
+        logger.info("OK");
+        return null;
+    }
+}
