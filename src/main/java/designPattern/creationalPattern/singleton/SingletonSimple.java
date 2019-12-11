9d3ba934fa39c29ff7bad30e8631114bbf39e742
Xiaofei <13263397018@163.com>
2019-12-11 10:43:20 +0800

    add design demo

---
+++ src/main/java/designPattern/creationalPattern/singleton/SingletonSimple.java
@@ -0,0 +1,12 @@
+package designPattern.creationalPattern.singleton;
+
+/**
+ * @ClassName SingletonSimple
+ * @Description Todo
+ * @AUTHOR xingxf03
+ * @Date 2019/10/9 16:44
+ * @Version v1.0
+ **/
+public class SingletonSimple {
+
+}
